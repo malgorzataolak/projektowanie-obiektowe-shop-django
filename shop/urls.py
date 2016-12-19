0d01d434e7a3cd921621be3ff1a760b459a6b83a
Małgorzata Olak <Magorzata Olak>
2016-12-19 15:43:25 +0100

    Dodanie pustych szablonow oraz zaimpementowanie przekazywania plikow obrazow

--- shop/urls.py
+++ shop/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -20,3 +22,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('myshop.urls', namespace='myshop')),
 ]
+
+if settings.DEBUG:
+	urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
