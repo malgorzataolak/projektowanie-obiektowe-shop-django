5c0b45228a4788f6c415a2ef4fff1132a1d28e3b
Małgorzata Olak <Magorzata Olak>
2017-01-16 23:12:32 +0100

    Naniesienie poprawek do szablonu CSS, usuniecie zbednego kodu

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def register(request):
@@ -17,22 +17,3 @@ def register(request):
 	return render(request, 'account/register.html', {'user_form':user_form})
 
 
-
-def user_login(request):
-	if request.method=='POST':
-		form=LoginForm(request.POST)
-		if form.is_valid():
-			cd=form.cleaned_data
-			user=authenticate(username=cd['username'], password=cd['password'])
-
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					return HttpResponse('Uwierzytelnienie zakończyło się sukcesem')
-				else:
-					return HttpResponse('Konto jest zablokowane')
-			else:
-				return HttpResponse('Nieprawidłowe dane uwierzytelniające')
-	else:
-		form=LoginForm()
-	return render(request, 'account/login.html', {'form': form})
